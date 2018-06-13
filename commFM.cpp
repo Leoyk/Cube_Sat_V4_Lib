b156b0b1ed3a2cc650ffef707559ea9a0c2a4cbe
Leoyk <leo.yk@qq.com>
2018-06-13 17:04:39 +0800

    Update commFM.cpp

--- commFM.cpp
+++ commFM.cpp
@@ -3,11 +3,11 @@
 #include "SoftwareSerial.h"
 
 
-SoftwareSerial comm = SoftwareSerial(50,51);//R T
+SoftwareSerial comm = SoftwareSerial(A10,A11);//R T
 // 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
 
-String fre="AT+FRE=";
-String vol="AT+VOL=";
+static String fre="AT+FRE=";
+static String vol="AT+VOL=";
 
 void FMinit(){
   comm.begin(38400);    
@@ -15,8 +15,10 @@ void FMinit(){
 }
 
 void setFre(int a){
-  comm.println(fre+(String)a);
+  a = constrain(a, 870, 1080);
+  comm.println(fre + String(a));
 }
 void setVol(int a){
-  comm.println(vol+(String)a);
+  a = constrain(a, 0, 30);
+  comm.println(vol + String(a));
 }
