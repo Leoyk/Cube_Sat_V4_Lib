0db91a635438f113bae205ee1e21ffafb49bd3d3
Leoyk <gmboxYK@gmail.com>
2018-06-15 18:43:54 +0800

    Signed-off-by: Leoyk <gmboxYK@gmail.com>

--- commBMP180.cpp
+++ commBMP180.cpp
@@ -1,107 +1,216 @@
-#include <commBMP180.h>
-#include <SFE_BMP180.h>
+
+//static for PT
 
 
-SFE_BMP180 pressure;// 创建一个气压计对象
+#include "commBMP180.h"
+#include "Arduino.h"
 
-double baseline; // 基准气压
 
+#define BMP180ADD 0x77  // I2C address of BMP180  
+                       
+  // Oversampling Setting           0: single    1: 2 times    2: 4 times   3: 8 times 
+#define OSS 3      
+//修改此处需要修改延时时间
+  
+  
+  
+/**********************MSB      LSB******/
+static int ac1;           // 0xAA     0xAB
+static int ac2;           // 0xAC     0xAD
+static int ac3;           // 0xAE     0xAE
+static unsigned int ac4;  // 0xB0     0xB1
+static unsigned int ac5;  // 0xB2     0xB3
+static unsigned int ac6;  // 0xB4     0xB5
+static int b1;            // 0xB6     0xB7
+static int b2;            // 0xB8     0xB9
+static int mb;            // 0xBA     0xBB
+static int mc;            // 0xBC     0xBD
+static int md;            // 0xBE     0xBF
+static long b5;          
 
 
 
 
 void pressureInit(){
-	pressure.begin();
-  //获得基准气压
-	baseline = getP();	
+  ac1 = bmp180ReadDate(0xAA);                      //get full data
+  ac2 = bmp180ReadDate(0xAC);  
+  ac3 = bmp180ReadDate(0xAE);  
+  ac4 = bmp180ReadDate(0xB0);  
+  ac5 = bmp180ReadDate(0xB2);  
+  ac6 = bmp180ReadDate(0xB4);  
+  b1  = bmp180ReadDate(0xB6);  
+  b2  = bmp180ReadDate(0xB8);  
+  mb  = bmp180ReadDate(0xBA);  
+  mc  = bmp180ReadDate(0xBC);  
+  md  = bmp180ReadDate(0xBE);
+  
+  
+//get baseline  
+  
 }
-
-double pressureVal(){
-	return getP();	
-}
-
-
-double altitudeVal(){
-  return pressure.altitude(getP(),baseline);//获得基于基准气压的高度值
-}
-
-double tempVal(){
-  return getT();
-}
+/*
+  temperature = bmp180GetTemperature(bmp180ReadUT());
+  temperature = temperature*0.1;
+  pressure = bmp180GetPressure(bmp180ReadUP());
+  pressure2 = pressure/101325;
+  pressure2 = pow(pressure2,0.19029496);
+  altitude = 44330*(1-pressure2);                            //altitude = 44330*(1-(pressure/101325)^0.19029496);
+*/
 
 
-
-
-
-
-
-
-
-double getP()
+/***BMP180 temperature Calculate***/
+float tempVal()
 {
-  char status;
-  double T,P,p0,a;
-  // You must first get a temperature measurement to perform a pressure reading.
-  // Start a temperature measurement:
-  // If request is successful, the number of ms to wait is returned.
-  // If request is unsuccessful, 0 is returned.
-  status = pressure.startTemperature();
-  if (status != 0)
-  {
-    // Wait for the measurement to complete:
-    delay(status);
-    // Retrieve the completed temperature measurement:
-    // Note that the measurement is stored in the variable T.
-    // Use '&T' to provide the address of T to the function.
-    // Function returns 1 if successful, 0 if failure.
-
-    status = pressure.getTemperature(T);
-    if (status != 0)
-    {
-      // Start a pressure measurement:
-      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
-      // If request is successful, the number of ms to wait is returned.
-      // If request is unsuccessful, 0 is returned.
-      status = pressure.startPressure(3);
-      if (status != 0)
-      {
-        // Wait for the measurement to complete:
-        delay(status);
-        // Retrieve the completed pressure measurement:
-        // Note that the measurement is stored in the variable P.
-        // Use '&P' to provide the address of P.
-        // Note also that the function requires the previous temperature measurement (T).
-        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
-        // Function returns 1 if successful, 0 if failure.
-        status = pressure.getPressure(P,T);
-        if (status != 0)
-        {
-          return P;
-        }
-        else Serial.println("error retrieving pressure measurement\n");
-      }
-      else Serial.println("error starting pressure measurement\n");
-    }
-    else Serial.println("error retrieving temperature measurement\n");
-  }
-  else Serial.println("error starting temperature measurement\n");
+  static long x1, x2;
+  unsigned int ut = getTV();
+  
+  x1 = 0;
+  x2 = 0;
+  
+  x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;  //x1=((ut-ac6)*ac5)/(2^15)
+  x2 = ((long)mc << 11)/(x1 + md);                //x2=(mc*2^11)/(x1+md)
+  b5 = x1 + x2;                                   //b5=x1+x2
+  return (((b5 + 8)>>4)*0.1);                           //t=(b5+8)/(2^4)
 }
 
-double getT()
+/***BMP180 pressure Calculate***/
+
+double pressureVal()
 {
-  char status;
-  double T,p0;
-  status = pressure.startTemperature();
-  if (status != 0)
-  {
-    delay(status);
-    status = pressure.getTemperature(T);
-    if (status != 0)
-    {
-      status = pressure.startPressure(3);
-      return T;
-    }
-    else Serial.println("error retrieving temperature measurement\n");
-  }
-  else Serial.println("error starting temperature measurement\n");
+  static long x1, x2, x3, b3, b6, p;
+  static unsigned long b4, b7;
+  
+  unsigned long up = getPV();
+  
+  x1= x2= x3= b3= b6= p=0;
+  b4= b7= 0;  
+  
+  
+  
+  b6 = b5 - 4000;
+
+  x1 = (b2 * (b6 * b6)>>12)>>11;
+  x2 = (ac2 * b6)>>11;
+  x3 = x1 + x2;
+  b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+  
+  x1 = (ac3 * b6)>>13;
+  x2 = (b1 * ((b6 * b6)>>12))>>16;
+  x3 = ((x1 + x2) + 2)>>2;
+  b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+  
+  b7 = ((unsigned long)(up - b3) * (50000>>OSS));
+  if (b7 < 0x80000000)
+    p = (b7<<1)/b4;
+  else
+    p = (b7/b4)<<1;
+    
+  x1 = (p>>8) * (p>>8);
+  x1 = (x1 * 3038)>>16;
+  x2 = (-7357 * p)>>16;
+  p += (x1 + x2 + 3791)>>4;
+  
+  return p;
 }
+
+
+
+// Altitude =(44330.0 * (1.0-pow((float)(pressure) / 101325, 1.0/5.255)) ); 
+double altitudeVal(double pre){
+  
+  double pre2;
+  pre2 = pre/101325;
+  pre2 = pow(pre2,0.19029496);
+  
+  return 44330*(1-pre2); 
+}
+
+
+
+/*** Read 1 bytes from the BMP180  ***/
+
+int bmp180Read(unsigned char address)
+{
+  unsigned char data;
+  
+  Wire.beginTransmission(BMP180ADD);
+  Wire.write(address);
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP180ADD, 1);
+  while(!Wire.available());
+    
+  return Wire.read();
+}
+
+/*** Read 2 bytes from the BMP180 ***/
+int bmp180ReadDate(unsigned char address)
+{
+  unsigned char msb, lsb;
+  Wire.beginTransmission(BMP180ADD);
+  Wire.write(address);
+  Wire.endTransmission();
+  Wire.requestFrom(BMP180ADD, 2);
+  while(Wire.available()<2);
+  msb = Wire.read();
+  lsb = Wire.read();
+  return (int) msb<<8 | lsb;
+}
+
+
+
+
+
+
+
+void orderTV()
+{
+  Wire.beginTransmission(BMP180ADD);
+  Wire.write(0xF4);                       // Write 0x2E into Register 0xF4
+  Wire.write(0x2E);                       // This requests a temperature reading
+  Wire.endTransmission();  
+  }
+
+// delay(5);                               // Wait at least 4.5ms
+  
+/*** read uncompensated temperature value ***/
+unsigned int getTV()
+{
+  unsigned int ut;
+
+  ut = bmp180ReadDate(0xF6);               // read MSB from 0xF6 read LSB from (16 bit)
+  return ut;
+}
+
+
+
+
+
+void orderPV(){//delay 8,dependent on oss
+  Wire.beginTransmission(BMP180ADD);
+  Wire.write(0xF4);                        // Write 0x34+(OSS<<6) into register 0xF4
+  Wire.write(0x34 + (OSS<<6));             // 0x34+oss*64
+  Wire.endTransmission(); 
+}
+
+  
+/*** Read uncompensated pressure value from BMP180 ***/
+unsigned long getPV()
+{
+  unsigned char msb, lsb, xlsb;
+  unsigned long up = 0;
+  
+
+  Wire.beginTransmission(BMP180ADD);
+  Wire.write(0xF6);                        // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
+  Wire.endTransmission();
+  
+  Wire.requestFrom(BMP180ADD, 3); 
+  while(Wire.available() < 3);             // Wait for data to become available
+  msb = Wire.read();
+  lsb = Wire.read();
+  xlsb = Wire.read();
+  up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);//16 to 19 bit
+  return up;
+}
+
--- commBMP180.h
+++ commBMP180.h
@@ -1,13 +1,30 @@
 #ifndef _COMMBMP180_H__
 #define _COMMBMP180_H__
+#include"Wire.h"
+
+// Oversampling Setting           0: single    1: 2 times    2: 4 times   3: 8 times 
+
 
 void pressureInit();
-double pressureVal();
-double altitudeVal();
-double tempVal();
-double getP();
-double getT();
 
+void orderPV();//延时30ms之后才能获取压力值
+double pressureVal();//获取到的气压值 单位：Pa
+
+
+double altitudeVal(double pre);//传入气压换算海拔高度值 单位：m
+
+
+void orderTV();//延时5ms之后才能获取温度值
+float tempVal();//获取到的温度值 单位：℃
+
+
+
+unsigned long getPV();
+unsigned int getTV();
+
+
+int bmp180Read(unsigned char address);
+int bmp180ReadDate(unsigned char address);
 
 
 #endif
